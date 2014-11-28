9f211fa08a6e9b8057d11d86534a7308f9cd3f68
Donald Morris <dfmorrisjr@globalsolutionsdev.com>
2014-11-28 12:20:58 -0600

    adding user with .m ingored

---
+++ ACAlerts/User.h
@@ -0,0 +1,16 @@
+//
+//  User.h
+//  ACAlerts
+//
+//  Created by Tammy Morris on 11/28/14.
+//  Copyright (c) 2014 Global Solutions Development. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@property (nonatomic, strong) NSString *username;
+@property (nonatomic, strong) NSString *password;
+
+@end
